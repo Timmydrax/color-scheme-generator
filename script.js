46062767bf76539537bbe10c5abe689cdcc49a34
Timmydrax <timmydrax@gmail.com>
2025-07-06 03:10:09 +0100

    CHORE: Implemented responsiveness

--- script.js
+++ script.js
@@ -3,30 +3,37 @@ const colorPicker = document.getElementById("seed-color");
 const schemeMode = document.getElementById("scheme-mode");
 const paletteContainer = document.getElementById("palette-container");
 
+// Form Event Listener
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
   const seedColor = colorPicker.value.substring(1);
   const mode = schemeMode.value;
 
-  //   try{
-  //     const response = await
-  //   }
-
   fetch(
     `https://www.thecolorapi.com/scheme?hex=${seedColor}&format=json&mode=${mode}&count=6`
   )
     .then((res) => res.json())
     .then((data) => {
-      const colors = data.colors;
-      const color = colors.map((color) => {
-        return `
-        <p>${color.hex.value} </p>
-        `;
+      const colors = data.colors.map((color) => {
+        return color.hex.value;
       });
-
-      console.log(color);
+      console.log(colors);
+      displayPalette(colors);
     });
 
   console.log(`the seed color ${seedColor}, and the schemeMode is ${mode}`);
 });
+
+function displayPalette(colors) {
+  paletteContainer.innerHTML = "";
+
+  colors.forEach((color) => {
+    const div = document.createElement("div");
+    div.className = "color-box";
+    div.style.backgroundColor = color;
+    div.innerHTML = `<span>${color}</span>`;
+
+    paletteContainer.appendChild(div);
+  });
+}
