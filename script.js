9ef4979ee7a42104f4caeb66fe80ba6e0c91361c
Timmydrax <timmydrax@gmail.com>
2025-07-06 02:14:16 +0100

    TEST: Testing API

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
- 
+const form = document.getElementById("form");
+const colorPicker = document.getElementById("seed-color");
+const schemeMode = document.getElementById("scheme-mode");
+const paletteContainer = document.getElementById("palette-container");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const seedColor = colorPicker.value.substring(1);
+  const mode = schemeMode.value;
+
+  //   try{
+  //     const response = await
+  //   }
+
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${seedColor}&format=json&mode=${mode}&count=6`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      const colors = data.colors;
+      const color = colors.map((color) => {
+        return `
+        <p>${color.hex.value} </p>
+        `;
+      });
+
+      console.log(color);
+      //   console.log(data);
+    });
+
+  console.log(`the seed color ${seedColor}, and the schemeMode is ${mode}`);
+});
